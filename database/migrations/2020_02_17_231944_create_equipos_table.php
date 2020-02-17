2e50c523a45e4ad021598cdacc7a44ad8fc95af8
David Soriano <david.sorianoc@gmail.com>
2020-02-17 17:27:05 -0600

    Table equipos

--- database/migrations/2020_02_17_231944_create_equipos_table.php
+++ database/migrations/2020_02_17_231944_create_equipos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTareasTable extends Migration
+class CreateEquiposTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateTareasTable extends Migration
      */
     public function up()
     {
-        Schema::create('tareas', function (Blueprint $table) {
+        Schema::create('equipos', function (Blueprint $table) {
             $table->bigIncrements('id');
-            
+            $table->string('nombre');
         });
     }
 
@@ -26,6 +26,6 @@ class CreateTareasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tareas');
+        Schema::dropIfExists('equipos');
     }
 }
